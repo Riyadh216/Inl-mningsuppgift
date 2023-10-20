5f638a89fd0076f449997b88b613dea94dfdb8d3
Admin <Admin@TEB123020>
2023-10-20 15:10:33 +0200

    Add project files.

---
+++ Inlämningsuppgift/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Inlämningsuppgift_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hur gammal är du? (år)");
+            int ålder = int.Parse(Console.ReadLine());
+            if (ålder > 16 && ålder  < 19)
+            {
+                Console.WriteLine("du får delta");
+            }
+            else
+            { 
+                Console.WriteLine("du får inte delta");
+
+            }
+        }
+    }
+}
